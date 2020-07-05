371fe9f0cfbe82b1f3747372772f4246476055bf
Keenan Ray <keenan.ray87@gmail.com>
2020-07-05 07:53:55 -0400

    added jwt authentication

---
+++ client/src/auth/auth-helper.js
@@ -0,0 +1,22 @@
+const authenticate = (jwt, cb) => {
+  if (typeof window !== 'undefined') {
+    localStorage.setItem('jwt', JSON.stringify(jwt));
+    cb();
+  }
+};
+
+const isAuthenticated = () => {
+  if (typeof window == 'undefined') {
+    return false;
+  }
+  if (localStorage.getItem('jwt')) {
+    return JSON.parse(localStorage.getItem('jwt'));
+  } else {
+    return false;
+  }
+};
+
+const clearJWT = () => {
+  if (typeof window != 'undefined') localStorage.removeItem('jwt');
+  cb();
+};
